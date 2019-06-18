3b37e52043c774d4c9dad7214bf375adf5df8c6e
unknown <hizialuke@gmail.com>
2019-06-18 17:40:18 +0700

    First Commit

---
+++ Assets/Credit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour {
+
+    public void showMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Warisan Nusantara
+
+Developed by :
+- Raham Sutan Ilyas (161111025).
+- Bimo Prakoso (161111030).
+- Hizkia Luke Susanto (161111044).
+- Muhamad Irfan Zidni (161111067).
