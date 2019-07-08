86352fd03c1fe81c559330d6513fc1bdf28c15e8
digiponic-raham <raham@digiponic.co.id>
2019-07-08 20:22:04 +0700

    PUSH YEHAA

---
+++ Assets/Level/HitSoundB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSoundB : MonoBehaviour {
+    public AudioClip impact;
+    AudioSource audioSource;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    public void SoundHitB() {
+        audioSource.PlayOneShot(impact, 0.3f);
+    }
+}
