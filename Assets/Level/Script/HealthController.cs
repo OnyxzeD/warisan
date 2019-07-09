762a88d83f640e4d0ffb3c1be9c48ec301e31ecf
digiponic-raham <raham@digiponic.co.id>
2019-07-09 15:08:00 +0700

    Folder Cleaning

---
+++ Assets/Level/Script/HealthController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+
+    public int HealthH,HealthB;
+    public Text healthTextH;
+    public Text healthTextB;
+    public BossAnimController bossAnimC;
+    public HeroAnimController heroAnimC;
+
+    // Use this for initialization
+    void Start () {
+        UpdateHealthB();
+        UpdateHealthH();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (HealthB <= 0)
+        {
+            bossAnimC.BossDead();
+        }
+	}
+
+    public void DamageToH()
+    {
+        HealthH -= 10;
+        UpdateHealthH();
+    }
+    public void DamageToB()
+    {
+        HealthB -= 10;
+        UpdateHealthB();
+    }
+
+    public void UpdateHealthH()
+    {
+        healthTextH.text = HealthH.ToString("0");
+    }
+
+    public void UpdateHealthB()
+    {
+        healthTextB.text = HealthB.ToString("0");
+    }
+}
---
+++ Assets/Level/Script/HeroAnimController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HeroAnimController : MonoBehaviour
+{
+
+    public Animator anim;
+    public HitSoundH hitH;
+    public HealthController healthController;
+    //private DamageControl damageCon = new DamageControl();
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            HeroAttack();
+        }
+    }
+
+    public void HeroAttack()
+    {
+        anim.Play("AttackH");
+        hitH.SoundHitH();
+        healthController.DamageToB();
+    }
+
+    public void HeroDead()
+    {
+        
+    }
+
+}
